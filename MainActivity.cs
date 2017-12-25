ca04eee4cd037b36f33dbb48ccd19bcb507fdb66
zhwb9190 <32408342+zhwb9190@users.noreply.github.com>
2017-12-25 11:04:56 +0800

    Add files via upload

---
+++ MainActivity.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Views;
+namespace wms
+{
+    [Activity(Label = "@string/app_name", LaunchMode = Android.Content.PM.LaunchMode.Multiple, Icon = "@drawable/icon")]
+    public class MainActivity : AppCompatActivity
+    {
+        Android.Support.V4.Widget.DrawerLayout _drawerLayout;
+        ListView listview_leftMenu;
+        Android.Support.V7.Widget.Toolbar toolbar;
+        ActionBarDrawerToggle _drawerToggle;
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            //Set your main view here
+            SetContentView(Resource.Layout.main);
+            ProgressBar bar = FindViewById<ProgressBar>(Resource.Id.progressBar1);
+            Button b_confirm = FindViewById<Button>(Resource.Id.button1);
+            Button b_exit = FindViewById<Button>(Resource.Id.button2);
+            b_confirm.Click += B_confirm_Click;
+            b_exit.Click += (Object sender, EventArgs e) =>
+            {
+                //对话框
+                var exitDialog = new Android.Support.V7.App.AlertDialog.Builder(this);
+                //对话框内容
+                exitDialog.SetMessage("Are you sure exit?");
+                //拨打按钮
+                exitDialog.SetNeutralButton("Yes", delegate
+                {
+                    Finish();
+                });
+                //取消按钮
+                exitDialog.SetNegativeButton("Cancel", delegate { });
+                //显示对话框
+                exitDialog.Show();
+            };
+        }
+
+        private void B_confirm_Click(object sender, EventArgs e)
+        {
+            ProgressBar bar = FindViewById<ProgressBar>(Resource.Id.progressBar1);
+            EditText user_id = FindViewById<EditText>(Resource.Id.editText1);
+            EditText user_password = FindViewById<EditText>(Resource.Id.editText2);
+            WebApi api = new WebApi();
+            if (user_id.Text.Trim() == "")
+            {
+                Toast.MakeText(this, "You must entry a valid username", ToastLength.Short).Show();
+                user_id.RequestFocus();
+                return;
+            }
+            if (user_password.Text.Trim() == "")
+            {
+                Toast.MakeText(this, "You must entry a valid password", ToastLength.Short).Show();
+                user_password.RequestFocus();
+                return;
+            }
+            if (api.Login(user_id.Text.Trim(), user_password.Text.Trim()))
+            {
+                SetContentView(Resource.Layout.menu);
+                
+                _drawerLayout = FindViewById<Android.Support.V4.Widget.DrawerLayout>(Resource.Id.dl_left);
+                listview_leftMenu = FindViewById<ListView>(Resource.Id.left_menu);
+                toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+                _drawerToggle = new ActionBarDrawerToggle(this, _drawerLayout, toolbar, 0, 0);//并没有起效果性作用
+                string[] menus = new string[] { "首页", "博问", "闪存" };
+                ArrayAdapter adapter = new ArrayAdapter(this, Android.Resource.Layout.SimpleExpandableListItem1, menus);
+                listview_leftMenu.Adapter = adapter;
+
+                toolbar.Title = "Toolbar1";
+                SetSupportActionBar(toolbar);//设置兼容toolbar，替代原本的actionbar
+
+                //SupportActionBar.SetDisplayShowHomeEnabled(true);//设置显示左上角Home图标
+                //SupportActionBar.SetDisplayHomeAsUpEnabled(true);//设置左上角的左箭头; 这两个必须同时为true才能显示
+                SupportActionBar.SetDisplayShowTitleEnabled(true);//设置不显示标题
+                SupportActionBar.SetHomeButtonEnabled(true);//设置返回键可用 
+                SupportActionBar.Title = "Toolbar";
+                toolbar.Title = "Toolbar1";
+                toolbar.SetTitleTextColor(Resources.GetColor(Resource.Color.white));
+
+                _drawerLayout.SetDrawerListener(_drawerToggle); //设置侧滑监听
+                _drawerToggle.SyncState(); //设置左箭头与Home图标的切换与侧滑同步
+                StatusBarUtil.SetColorStatusBar(this);
+            }
+            else
+            {
+                bar.Visibility = ViewStates.Invisible;
+                Toast.MakeText(this, "user name or password is not valid", ToastLength.Short).Show();
+            }
+        }
+    //图标动画效切换的关键：响应action 按钮的点击事件，包括左侧系统的home按钮，left按钮，右侧自定义的菜单等
+    public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            return base.OnOptionsItemSelected(item) || _drawerToggle.OnOptionsItemSelected(item);
+            //_drawerToggle.OnOptionsItemSelected(item)兼容android5.0以下的左侧图标切换的动画,不加这句android5.0以下的左上侧按钮无效
+        }
+    }
+}
+
+
+
+
+
+
+
---
+++ WebApi.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using System.Net;
+using System.Threading;
+namespace wms
+{
+    public class WebApi
+    {
+        /// <summary>
+        /// 系统登陆验证
+        /// </summary>
+        /// <returns></returns>
+        public bool Login(string user_id, string password)
+        {
+            bool flag = false;
+            try
+            {
+                #region requestJson
+                StringBuilder reqJson = new StringBuilder();
+                reqJson.Append("{");
+                reqJson.Append("\"User_id\": \"" + user_id + "\",");
+                reqJson.Append("\"User_password\": \"" + password + "\"");
+                reqJson.Append("}");
+                #endregion
+                string strJson = reqJson.ToString();
+                RestClient client = new RestClient(@"http://192.168.0.56/apitest/api/User");
+                RestRequest request = new RestRequest(@"Login", Method.POST);
+                var json = JObject.Parse(strJson);
+                request.AddParameter("application/json; charset=utf-8", json, ParameterType.RequestBody);
+                request.RequestFormat = DataFormat.Json;
+                IRestResponse resp = client.Execute(request);
+                if (resp.StatusCode == HttpStatusCode.OK)
+                {
+                    var result = resp.Content;
+                    var jResult = JObject.Parse(result);
+                    if (jResult["status"].ToString() == "1")
+                        flag = true;
+                }
+                return flag;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
